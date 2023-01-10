bd3634d7871e6f0bcffeed39bdd9bd2c5a66c8ae
Ronny <daveavrosia@gmail.com>
2023-01-10 22:30:13 +0800

    Add task routes

--- app/routes/v1/index.js
+++ app/routes/v1/index.js
@@ -1,5 +1,6 @@
 import sampleRoute from "./sample/sample.route.js";
 import usersRoute from "./users/users.route.js";
 import authRoute from "./auth/auth.route.js";
+import tasksRoute from "./tasks/tasks.route.js";
 
-export { sampleRoute, usersRoute, authRoute };
+export { sampleRoute, usersRoute, authRoute, tasksRoute };
