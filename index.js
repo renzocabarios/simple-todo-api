303541a00e18ebf1e30672918fba95db4c70d0ae
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-01-10 21:37:00 +0800

    Add docker for mongodb

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-const express = require('express');
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import connectDB from "./app/db/index.js";
+import ENV from "./app/env/index.js";
+import { sampleRoute } from "./app/routes/v1/index.js";
+
 const app = express();
-const port = 3000;
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-console.log("hello world!");
+
+// middlewares
+app.use(bodyParser.json(), bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+//routes
+app.use("/api/v1/sample", sampleRoute);
+
+//initialization
+const start = () => {
+  connectDB(ENV.MONGO_CON).then(() => {
+    console.log(`Database connected to ${ENV.MONGO_CON}`);
+
+    app.listen(ENV.PORT, () => {
+      console.log(`Server started on port ${ENV.PORT}`);
+    });
+  });
+};
+
+start();
